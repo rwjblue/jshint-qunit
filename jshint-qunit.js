2d6e08e94ef1ff0f6d5ce8beef06c0e263edc165
Robert Jackson <robert.w.jackson@me.com>
2014-04-21 23:03:56 -0400

    Initial commit.

---
+++ jshint-qunit.js
@@ -0,0 +1,51 @@
+function jsHintReporter(file, errors) {
+  if (!errors) { return ''; }
+
+  var len = errors.length,
+  str = '',
+  error, idx;
+
+  if (len === 0) { return ''; }
+
+  for (idx=0; idx<len; idx++) {
+    error = errors[idx];
+    str += file  + ': line ' + error.line + ', col ' +
+      error.character + ', ' + error.reason + '\n';
+  }
+
+  return str + "\n" + len + ' error' + ((len === 1) ? '' : 's');
+};
+
+function JSHintQunit(options) {
+  if (!(this instanceof JSHintQunit)) return new JSHintQunit(options);
+
+  this._jshintrc = options.jshintrc;
+  this.registry  = options.registry || requirejs.entries;
+  this.qunit     = options.qunit || QUnit;
+
+  this.qunit.config.urlConfig.push('nojshint');
+  this.enabled   = !this.qunit.urlParams.nojshint;
+};
+
+JSHintQunit.prototype.generateTests = function () {
+  var _this = this;
+
+  for (var moduleName in _this.registry) {
+    if (!_this.registry.hasOwnProperty(moduleName)) { continue; }
+
+    _this.addTest(moduleName, _this.registry[moduleName].callback.toString());
+  }
+};
+
+JSHintQunit.prototype.addTest = function(moduleName, contents) {
+  var _this = this;
+
+  module(moduleName);
+  test('should pass jshint', function() {
+    var passed = JSHINT(contents, _this.jshintrc),
+    errors = jsHintReporter(moduleName, JSHINT.errors);
+    ok(passed, moduleName+" should pass jshint."+(errors ? "\n"+errors : ''));
+  });
+};
+
+window.JSHintQunit = JSHintQunit;
